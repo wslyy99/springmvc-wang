326c114fd3e32bd52991ee17961c1157671fc1af
wslyy99 <wslyy99@163.com>
2017-05-19 10:36:19 +0800

    Update BizException.java

--- common-wang/wang-common-core/src/main/java/com/wang/common/exception/BizException.java
+++ common-wang/wang-common-core/src/main/java/com/wang/common/exception/BizException.java
@@ -1,24 +1,7 @@
-/*
- * Copyright 2015-2102 RonCoo(http://www.roncoo.com) Group.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.wang.common.exception;
 
 /**
  * 业务异常基类，所有业务异常都必须继承于此异常 .
- * @company：广州领课网络科技有限公司（龙果学院 www.roncoo.com）.
- * @author along
  */
 public class BizException extends RuntimeException {
 
