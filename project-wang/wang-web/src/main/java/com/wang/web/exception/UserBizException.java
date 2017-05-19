3723c3a060f5b4a7cdd055841b6c9aed7d8f0a61
wslyy99 <wslyy99@163.com>
2017-05-19 10:39:48 +0800

    Update UserBizException.java

--- project-wang/wang-web/src/main/java/com/wang/web/exception/UserBizException.java
+++ project-wang/wang-web/src/main/java/com/wang/web/exception/UserBizException.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015-2102 RonCoo(http://www.roncoo.com) Group.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.wang.web.exception;
 
 import com.wang.common.exception.BizException;
@@ -21,7 +6,6 @@ import com.wang.common.exception.BizException;
 
 /**
  * 用户业务异常类
- * @author：Peter
  */
 public class UserBizException extends BizException {
 
@@ -33,10 +17,10 @@ public class UserBizException extends BizException {
 	/** 用户不存在 **/
     public static final int USER_IS_NULL = 101;
 
-    /** 用户支付配置有误 **/
-    public static final int USER_PAY_CONFIG_ERRPR = 102;
+    /** 用户操作有误 **/
+    public static final int USER_OPER_ERRPR = 102;
     
-    public static final UserBizException USER_BANK_ACCOUNT_IS_NULL = new UserBizException(10010002, "用户未设置银行账户信息!");
+    public static final UserBizException USER_ACCOUNT_IS_NULL = new UserBizException(10010002, "用户未设置账户信息!");
 
     private static final org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getLogger(UserBizException.class);
 
